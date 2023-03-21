3595f4d2afdfacbdc7f2c3fb87773be280d73705
ve111932 <esteban.vangi@etu.unice.fr>
2023-03-21 17:35:21 +0100

    Reconfiguration fichier vue

---
+++ discordfront/src/components/Inscription.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+      <h1>Content de te revoir!</h1>
+      <h2></h2>
+      <login-form>
+      </login-form>
+      <div>
+      </div>
+    </div>
+
+</template>
---
+++ discordfront/src/components/LoginPage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="login">
+      <h1>Content de te revoir!</h1>
+      <h2></h2>
+      <login-form></login-form>
+      <div>
+        <router-link to="/register">Créer un compte</router-link>
+        <router-link to="/forgot-password">Mot de passe oublié</router-link>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import LoginForm from './LoginForm.vue'
+  
+  export default {
+    components: {
+      LoginForm
+    }
+  }
+  </script>
