47be26e2e3fa1a0c05864d32058514338d6dcfad
Esteban <estebanvangi@icloud.com>
2023-04-16 19:59:52 +0200

    modification controlleur auth pour role

--- app/middleware/authJwt.js
+++ app/middleware/authJwt.js
@@ -25,19 +25,14 @@ verifyToken = (req, res, next) => {
 
 isAdmin = (req, res, next) => {
   Users.findByPk(req.userId).then(user => {
-    user.getRoles().then(roles => {
-      for (let i = 0; i < roles.length; i++) {
-        if (roles[i].name === "admin") {
-          next();
-          return;
-        }
-      }
-
-      res.status(403).send({
-        message: "Require Admin Role!"
-      });
+    if(user.role_id==2){
+      next();
       return;
+    }
+    res.status(403).send({
+      message: "Vous devez être administrateur pour pouvoir accéder a cette fonction!"
     });
+    return;    
   });
 };
 
