3a6d5e6d9f169097d036358c3fa586f1a0d85cd9
Esteban <estebanvangi@icloud.com>
2023-05-01 14:32:14 +0200

    Verification duplicata update user

--- app/middleware/verifySignUp.js
+++ app/middleware/verifySignUp.js
@@ -3,19 +3,24 @@ const Roles = db.Roles;
 const Users = db.Users;
 
 checkDuplicateUsernameOrEmail = (req, res, next) => {
-  // Username
-  Users.findOne({
-    where: {
-      username: req.body.username
-    }
-  }).then(user => {
-    if (user) {
-      res.status(400).send({
-        message: "Ce nom d'utilisateur existe déja!"
-      });
-      return;
-    }
-
+  
+  if(req.body.username){
+    // Username
+    Users.findOne({
+      where: {
+        username: req.body.username
+      }
+    }).then(user => {
+      if (user) {
+        res.status(400).send({
+          message: "Ce nom d'utilisateur existe déja!"
+        });
+        return;
+      }
+      next();
+    })
+  }
+  else if(req.body.email){
     // Email
     Users.findOne({
       where: {
@@ -24,16 +29,14 @@ checkDuplicateUsernameOrEmail = (req, res, next) => {
     }).then(user => {
       if (user) {
         res.status(400).send({
-          message: "Un compte avec cet email a déjà été crée!"
+          message: "Il existe déjà un compte avec cet email!"
         });
         return;
       }
-
       next();
     });
-  });
-};
-
+  }
+}
 checkRolesExisted = (req, res, next) => {
   if (req.body.role) {
       if (!Roles.includes(req.body.role)) {
